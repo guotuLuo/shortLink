7abf879d009b8feb55351444779847b8098b72c9
guotuluo <2403105429@qq.com>
2024-11-19 21:33:45 +0800

    develop: 开发短链接分组实体

---
+++ admin/src/main/java/com/nageoffer/shortLInk/admin/dao/entity/GroupDO.java
@@ -0,0 +1,34 @@
+package com.nageoffer.shortlink.admin.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.nageoffer.shortlink.admin.common.datbase.BaseDO;
+import lombok.*;
+
+@EqualsAndHashCode(callSuper = true)
+@TableName("t_group")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class GroupDO extends BaseDO {
+    /**
+     * ID
+     * */
+    private Long id;
+    /**
+     * 分组ID
+     * */
+    private String gid;
+    /**
+     * 分组名称
+     * */
+    private String name;
+    /**
+     * 创建分组用户名
+     * */
+    private String username;
+    /**
+     * 分组排序
+     * */
+    private Integer sortOrder;
+}
