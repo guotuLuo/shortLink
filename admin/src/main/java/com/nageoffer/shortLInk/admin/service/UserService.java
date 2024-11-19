03a17bbd76cb3541f6139f83e8c0f48d908938e0
guotuluo <2403105429@qq.com>
2024-11-19 21:36:42 +0800

    develop: 开发短链接用户登录注销服务功能

--- admin/src/main/java/com/nageoffer/shortLInk/admin/service/UserService.java
+++ admin/src/main/java/com/nageoffer/shortLInk/admin/service/UserService.java
@@ -2,12 +2,23 @@ package com.nageoffer.shortlink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.nageoffer.shortlink.admin.dao.entity.UserDO;
-import com.nageoffer.shortlink.admin.dto.req.UserRequestDTO;
+import com.nageoffer.shortlink.admin.dto.req.UserLoginReqDTO;
+import com.nageoffer.shortlink.admin.dto.req.UserRegisterReqDTO;
+import com.nageoffer.shortlink.admin.dto.req.UserUpdateReqDTO;
+import com.nageoffer.shortlink.admin.dto.resp.UserLoginRespDTO;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 
 public interface UserService extends IService<UserDO> {
     UserRespDTO getByUserName(String username);
     Boolean isAvailableUserName(String username);
 
-    void register(UserRequestDTO requestDTO);
+    void register(UserRegisterReqDTO requestDTO);
+
+    void update(UserUpdateReqDTO userUpdateDTO);
+
+    UserLoginRespDTO login(UserLoginReqDTO userLoginReqDTO);
+
+    Boolean checkLogin(String username, String uuid);
+
+    void logout(String username, String uuid);
 }
