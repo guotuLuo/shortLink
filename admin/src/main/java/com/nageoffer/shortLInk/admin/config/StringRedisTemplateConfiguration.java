3b2cc71ddb4de81ad628340aafb2b3d1f752e42d
guotuluo <2403105429@qq.com>
2024-11-19 21:31:24 +0800

    develop: redis自动装配Bean

---
+++ admin/src/main/java/com/nageoffer/shortLInk/admin/config/StringRedisTemplateConfiguration.java
@@ -0,0 +1,16 @@
+package com.nageoffer.shortlink.admin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.util.Assert;
+
+@Configuration
+public class StringRedisTemplateConfiguration {
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory){
+        Assert.notNull(redisConnectionFactory, "RedisConnectionFactory must not be null");
+        return new StringRedisTemplate(redisConnectionFactory);
+    }
+}
