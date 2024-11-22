2456f12dbda8721c427a59e69d5d9d56e9d1b5cf
guotuluo <2403105429@qq.com>
2024-11-22 10:37:45 +0800

    develop: 短链接分组服务层开发

--- admin/src/main/java/com/nageoffer/shortLInk/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/nageoffer/shortLInk/admin/service/impl/GroupServiceImpl.java
@@ -4,8 +4,11 @@ import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.nageoffer.shortlink.admin.biz.user.UserContext;
 import com.nageoffer.shortlink.admin.dao.entity.GroupDO;
 import com.nageoffer.shortlink.admin.dao.mapper.GroupMapper;
+import com.nageoffer.shortlink.admin.dto.req.GroupSortReqDTO;
+import com.nageoffer.shortlink.admin.dto.req.GroupUpdateReqDTO;
 import com.nageoffer.shortlink.admin.dto.resp.GroupQueryRespDTO;
 import com.nageoffer.shortlink.admin.service.GroupService;
 import com.nageoffer.shortlink.admin.toolkit.RandomGenerator;
@@ -27,7 +30,9 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
         GroupDO groupDO = GroupDO
                 .builder()
                 .gid(gid)
+                .username(UserContext.getUsername())
                 .name(groupName)
+                .sortOrder(0)
                 .build();
         baseMapper.insert(groupDO);
     }
@@ -35,19 +40,52 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
     private boolean availableGID(String gid) {
         LambdaQueryWrapper<GroupDO> eq = Wrappers.lambdaQuery(GroupDO.class)
                 .eq(GroupDO::getGid, gid)
-                .eq(GroupDO::getUsername, null);
-
+                .eq(GroupDO::getUsername, UserContext.getUsername());
         return baseMapper.selectOne(eq) == null;
-
     }
 
     @Override
     public List<GroupQueryRespDTO> queryList() {
         LambdaQueryWrapper<GroupDO> groupDOLambdaQueryWrapper = Wrappers.lambdaQuery(GroupDO.class)
-                .eq(GroupDO::getUsername, "carrot")
+                .eq(GroupDO::getUsername, UserContext.getUsername())
                 .eq(GroupDO::getDelFlag, 0)
                 .orderByDesc(GroupDO::getSortOrder, GroupDO::getUpdateTime);
         List<GroupDO> groupDOS = baseMapper.selectList(groupDOLambdaQueryWrapper);
         return BeanUtil.copyToList(groupDOS, GroupQueryRespDTO.class);
+    }
+
+    @Override
+    public void update(GroupUpdateReqDTO groupUpdateReqDTO) {
+        LambdaQueryWrapper<GroupDO> eq = Wrappers.lambdaQuery(GroupDO.class)
+                .eq(GroupDO::getUsername, UserContext.getUsername())
+                .eq(GroupDO::getGid, groupUpdateReqDTO.getGid())
+                .eq(GroupDO::getSortOrder, 0);
+        GroupDO groupDO = new GroupDO();
+        groupDO.setName(groupUpdateReqDTO.getName());
+        baseMapper.update(groupDO, eq);
+    }
+
+    @Override
+    public void delete(String gid) {
+        LambdaQueryWrapper<GroupDO> eq = Wrappers.lambdaQuery(GroupDO.class)
+                .eq(GroupDO::getUsername, UserContext.getUsername())
+                .eq(GroupDO::getGid, gid)
+                .eq(GroupDO::getDelFlag, 0);
+        GroupDO groupDO = new GroupDO();
+        groupDO.setDelFlag(1);
+        baseMapper.update(groupDO, eq);
+    }
+
+    @Override
+    public void sort(List<GroupSortReqDTO> list) {
+        list.forEach(each ->{
+            LambdaQueryWrapper<GroupDO> eq = Wrappers.lambdaQuery(GroupDO.class)
+                    .eq(GroupDO::getUsername, UserContext.getUsername())
+                    .eq(GroupDO::getGid, each.getGid());
+            GroupDO groupDO = new GroupDO();
+            groupDO.setSortOrder(each.getSortOrder());
+            baseMapper.update(groupDO, eq);
+        });
+
     }
 }
