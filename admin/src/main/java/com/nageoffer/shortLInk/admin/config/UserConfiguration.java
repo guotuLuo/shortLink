2b8eade12759de54b520ff8d9dfcf8b39e7debdd
guotuluo <2403105429@qq.com>
2024-11-22 10:40:00 +0800

    develop: 短链接用户上下文配置

---
+++ admin/src/main/java/com/nageoffer/shortLInk/admin/config/UserConfiguration.java
@@ -0,0 +1,23 @@
+package com.nageoffer.shortlink.admin.config;
+
+import com.nageoffer.shortlink.admin.biz.user.UserTransmitFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@Configuration
+@RequiredArgsConstructor
+public class UserConfiguration {
+    private final StringRedisTemplate stringRedisTemplate;
+    @Bean
+    public FilterRegistrationBean<UserTransmitFilter> globalUserTransMitFilter(){
+        FilterRegistrationBean<UserTransmitFilter> globalUserTransMitFilter = new FilterRegistrationBean<>();
+        globalUserTransMitFilter.setFilter(new UserTransmitFilter(stringRedisTemplate));
+        globalUserTransMitFilter.addUrlPatterns("/*");
+        globalUserTransMitFilter.addInitParameter("excludeUrl", "/api/shortlink/v1/user/login");
+        globalUserTransMitFilter.setOrder(0);
+        return globalUserTransMitFilter;
+    }
+}
