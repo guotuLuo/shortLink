677c287b4042366b7c8ae71063e1d9b8914fc48f
guotuluo <2403105429@qq.com>
2024-11-22 10:37:23 +0800

    develop: 短链接分组功能开发

--- admin/src/main/java/com/nageoffer/shortLInk/admin/service/GroupService.java
+++ admin/src/main/java/com/nageoffer/shortLInk/admin/service/GroupService.java
@@ -2,6 +2,8 @@ package com.nageoffer.shortlink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.nageoffer.shortlink.admin.dao.entity.GroupDO;
+import com.nageoffer.shortlink.admin.dto.req.GroupSortReqDTO;
+import com.nageoffer.shortlink.admin.dto.req.GroupUpdateReqDTO;
 import com.nageoffer.shortlink.admin.dto.resp.GroupQueryRespDTO;
 
 import java.util.List;
@@ -19,4 +21,10 @@ public interface GroupService extends IService<GroupDO> {
      * 查询分组
      * */
     List<GroupQueryRespDTO> queryList();
+
+    void update(GroupUpdateReqDTO groupUpdateReqDTO);
+
+    void delete(String gid);
+
+    void sort(List<GroupSortReqDTO> list);
 }
