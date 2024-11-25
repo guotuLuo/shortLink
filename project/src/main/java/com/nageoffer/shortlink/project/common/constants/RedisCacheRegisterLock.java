6fa12c4534e36d2129bb763baf7e85e9a493746f
guotuluo <2403105429@qq.com>
2024-11-25 21:47:43 +0800

    develop: redis锁名称

---
+++ project/src/main/java/com/nageoffer/shortlink/project/common/constants/RedisCacheRegisterLock.java
@@ -0,0 +1,5 @@
+package com.nageoffer.shortlink.project.common.constants;
+
+public class RedisCacheRegisterLock {
+    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
+}
