efcd322ac3e984f7dc378279b686b55df50d0fd4
guotuluo <2403105429@qq.com>
2024-11-25 21:42:49 +0800

    refactor: 公共类抽取

---
+++ project/src/main/java/com/nageoffer/shortlink/project/common/datbase/BaseDO.java
@@ -0,0 +1,23 @@
+package com.nageoffer.shortlink.project.common.datbase;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class BaseDO {
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+    /**
+     * 更新时间
+     * */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+    /**
+     * 删除标识0， 未删除标识1
+     * */
+    @TableField(fill = FieldFill.INSERT)
+    private Integer delFlag;
+}
