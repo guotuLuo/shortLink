b5197a70314ddfc4de75a9784945474ba5ea0c64
guotuluo <2403105429@qq.com>
2024-12-04 23:50:47 +0800

    develop: 优化短链接分组返回实体

--- admin/src/main/java/com/nageoffer/shortLInk/admin/dto/resp/GroupQueryRespDTO.java
+++ admin/src/main/java/com/nageoffer/shortLInk/admin/dto/resp/GroupQueryRespDTO.java
@@ -13,11 +13,11 @@ public class GroupQueryRespDTO {
      * */
     private String name;
     /**
-     * 创建分组用户名
-     * */
-    private String username;
-    /**
      * 分组排序
      * */
     private Integer sortOrder;
+    /**
+     * 当前分组短链接数量
+     * */
+    private Integer shortLinkCount;
 }
